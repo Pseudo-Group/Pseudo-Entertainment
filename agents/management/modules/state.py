300c56bbc5e3869ea9bf69822de3e1c93bb86cc6
Kimhansav <gene2002@naver.com>
2025-06-22 19:55:34 +0900

    Added diary_generation node, youtube_search node, youtube_analysis node

--- agents/management/modules/state.py
+++ agents/management/modules/state.py
@@ -22,9 +22,10 @@ class ManagementState(TypedDict):
     project_id: str  # 프로젝트 ID (예: "PRJ-2023-001", "EP-MARVEL-S01")
     request_type: str  # 요청 유형 (예: "resource_allocation", "team_management", "creator_development")
     query: str  # 사용자 쿼리 또는 요청사항
+    response: Annotated[
+        list, add_messages
+    ]
     team_members: Optional[List[str]] = None  # 팀 구성원 목록
     resources_available: Optional[Dict[str, any]] = None  # 사용 가능한 리소스 정보
     resource_plan: Optional[str] = None  # 리소스 계획 콘텐츠
-    response: Annotated[
-        list, add_messages
-    ]  # 응답 메시지 목록 (add_messages로 주석되어 메시지 추가 기능 제공)
+      # 응답 메시지 목록 (add_messages로 주석되어 메시지 추가 기능 제공)
--- agents/music/modules/nodes.py
+++ agents/music/modules/nodes.py
@@ -5,9 +5,63 @@
 """
 
 from agents.base_node import BaseNode
-from agents.music.modules.prompts import get_lyric_template
+from agents.music.modules.prompts import get_lyric_template, get_diary_template, get_query_extraction_template, get_youtube_query_template, get_youtube_analysis_template
 from agents.music.modules.state import MusicState
-from agents.music.modules.models import get_openai_model
+from agents.music.modules.models import get_openai_model, get_gemini_client, get_youtube_client
+
+from googleapiclient.discovery import build
+from google import genai
+
+class DiaryGenerationNode(BaseNode):
+    """
+    일기 생성 노드
+    """
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = get_openai_model()
+
+    def execute(self, state) -> dict:
+        """
+        일기 생성 노드 실행
+        """
+        self.logging("execute", input_state=state)
+        first_prompt = get_diary_template().format(query=state["query"])
+        diary_topic_response = self.model.invoke(first_prompt)
+        diary_topic_response = diary_topic_response.text()
+        
+        second_prompt = get_query_extraction_template().format(query=state["query"])
+        messages = [
+            {
+                "role" : "user",
+                "content" : first_prompt,
+            },
+            {
+                "role" : "assistant",
+                "content" : diary_topic_response,
+            },
+            {
+                "role" : "user",
+                "content" : second_prompt,
+            }
+        ]
+        query_response = self.model.invoke(messages)
+        query_response = query_response.text()
+        
+        # if "varying_data" not in state:
+        #     state["varying_data"] = {}
+        
+        # state["varying_data"]["youtube_query"] = query_response
+
+        # return {"response" : query_response, "query": state["query"], "varying" : state["varying_data"]}
+        return {"youtube_query" : query_response}
+
+    def __call__(self, state):
+        """
+        노드를 함수처럼 호출 가능하게 만드는 메서드
+        """
+        return self.execute(state)
+
 
 class LyricGenerationNode(BaseNode):
     """
@@ -33,7 +87,85 @@ class LyricGenerationNode(BaseNode):
         """
         return self.execute(state)
     
+
+class YoutubeSearchNode(BaseNode):
+    """
+    유튜브 영상을 찾는 노드
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = get_youtube_client()
+
+    def execute(self, state : MusicState) -> dict:
+        """
+        유튜브 영상 검색 노드 실행
+        """
+        self.logging("execute", input_state = state)
+        prompt = state["youtube_query"]
+        max_results = 1
+
+        search_response = self.model.search().list(
+            q = prompt,
+            part = "id", #영상 식별만 하면 돼서 id로 검색
+            type = "video", # 영상만 검색
+            order = "relevance", # 관련성 기준으로 정렬
+            maxResults = max_results
+        ).execute()
+
+        videos = []
+        # 검색 결과 파싱
+        for item in search_response.get('items', []):
+            video_id = str(item['id']['videoId'])
+            videos.append("https://www.youtube.com/watch?v=" + video_id)
         
+        # state["varying_data"]["video_url"] = videos[0]
+
+        # return {"response": videos[0], "query": state["query"], "varying" : state["varying_data"]} #검색 결과 영상들 중 첫 번째만 반환
+        return {"video_url" : videos[0]}
+
+    def __call__(self, state):
+        """
+        노드를 함수처럼 호출 가능하게 만드는 메서드
+        """
+        return self.execute(state)
+
+
+class YoutubeAnalysisNode(BaseNode):
+    """
+    유튜브 영상을 분석하는 노드
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = get_gemini_client()
+
+    def execute(self, state : MusicState) -> dict:
+        """
+        유튜브 영상 분석 노드 실행
+        """
+        self.logging("execute", input_state = state)
+        prompt = get_youtube_analysis_template().format(query=state["query"])
+        video_link = state["video_url"]
+        response = self.model.models.generate_content(
+            model='models/gemini-2.0-flash',
+            contents = genai.types.Content(
+            parts = [
+                genai.types.Part(
+                    file_data = genai.types.FileData(file_uri = video_link)
+                    ),
+                    genai.types.Part(text = prompt)
+                ]
+            )
+        )
+        # response = response["candidates"]["content"]["parts"][0]["text"]
+        # return {"response" : response, "query" : state["query"], "varying" : state["varying_data"]}
+        return {"video_analysis" : response.text}
+
+    def __call__(self, state):
+        """
+        노드를 함수처럼 호출 가능하게 만드는 메서드
+        """
+        return self.execute(state)
 
 
 # from agents.music.modules.chains import set_music_generation_chain
